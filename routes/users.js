6d88cc99eff986372c1503d391aa095e19f8ed69
Henno Täht <henno.taht@gmail.com>
2020-10-08 14:03:04 +0300

    As a user I can see my account details [Delivers #175181283]

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const User = require('../models/User')
 const Account = require('../models/Account')
 const bcrypt = require('bcrypt')
+const {verifyToken} = require('../middlewares')
+const userModel = require('../models/User')
+const accountModel = require('../models/Account')
 
 
 router.post('/', async (req, res, next) => {
@@ -48,6 +51,22 @@ router.post('/', async (req, res, next) => {
         res.status(409).send({error: e.message})
     }
 
+})
+
+router.get('/current', verifyToken, async (req, res, next) => {
+
+    // Get user object from DB
+    const user = await userModel.findOne({_id: req.userId})
+
+    // Get user's accounts
+    const accounts = await accountModel.find({userId: req.userId})
+
+    res.status(200).json({
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        accounts: accounts
+    })
 })
 
 module.exports = router;
