baa33c5b4ada15eb3fddf84fcb2d0566165b25e4
Henno Täht <henno.taht@gmail.com>
2020-10-15 18:14:33 +0300

    As a user I can transfer funds to another account [Delivers #175182313]

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const usersRoute = require('./routes/users')
 const sessionsRoute = require('./routes/sessions')
+const transactionsRoute = require('./routes/transactions')
 const mongoose = require('mongoose')
 const swaggerUi = require('swagger-ui-express')
 const yaml = require('yamljs')
 const swaggerDocument = yaml.load('docs/swagger.yaml')
+const {processTransactions} = require('./middlewares')
 
 // Copy env variables from .env file to process.env
 require('dotenv').config()
@@ -16,16 +18,19 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 // Attach routes
 app.use('/users', usersRoute)
+app.use('/transactions', transactionsRoute)
 app.use('/sessions', sessionsRoute)
 
 mongoose.connect(process.env.DB_CONNECT, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
-}, ()=>{
+}, () => {
     console.log('Heihoo');
 })
 
-app.listen(process.env.PORT, ()=>{
-    console.log('listening on http://localhost:'+process.env.PORT);
+processTransactions()
+
+app.listen(process.env.PORT, () => {
+    console.log('listening on http://localhost:' + process.env.PORT);
 })
